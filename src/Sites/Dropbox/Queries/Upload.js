4077110753ab1c5a97f12802addda13bd4501649
Gregory Goijaerts <crecketgaming@gmail.com>
2017-02-23 12:09:38 +0100

    rewrote dropbox upload to use promises

--- src/Sites/Dropbox/Queries/Upload.js
+++ src/Sites/Dropbox/Queries/Upload.js
@@ -27,110 +27,237 @@ module.exports = class Upload extends HelperInterface {
     handle(query) {
         // console.log(query);
         return new Promise((resolve, reject) => {
-            // first get user info
-            this._app._UserHelper.getUser(query.from.id).then((user_info) => {
-                if (!user_info) {
-                    return resolve("It looks like you're not registered in our system.");
-                }
+            // global helpers
+            let msgInfo;
+            let userInfo
 
-                if (!user_info.provider_sites.dropbox) {
-                    return resolve("Dropbox service not connected to your account");
-                }
+            // get information about this query user
+            this.getUser(query)
+                .then(resolveResults => {
+                    userInfo = resolveResults.userInfo;
 
-                // store key to fetch info about file
-                var storeKey = "upload_" + query.message.chat.id +
-                    "-" + query.message.message_id;
-
-                // fetch from cache
-                this._app._Cache.get(storeKey, (error, result) => {
-                    if (error) {
-                        return reject(error);
-                    }
-                    if (!result) {
-                        return resolve("We couldn't find a file connected to this message. " +
-                            "Try forwarding the file to UploadBro again so he can detect it more easily.");
-                    }
-                    var chat_id = result.chat_id;
-                    var message_id = result.message_id;
-                    var file_id = result.file_id;
-                    var file_name = result.file_name;
-
-                    // set initial message status
-                    this.editMessage("\u{1F50E} Fetching file... 1/3", {
-                        chat_id: chat_id,
-                        message_id: message_id
-                    }).then((result_message) => {
-                        // download the file from telegram
-                        this.downloadFile(file_id, chat_id, file_name)
-                            .then((file_location) => {
-                                // begin uploading to dropbox drive
-                                this.editMessage("\u{231B} Uploading to Dropbox... 2/3", {
-                                    chat_id: chat_id,
-                                    message_id: message_id
-                                }).then((result_message) => {
-
-                                    // get file contents
-                                    fs.readFile(file_location, (err, data) => {
-                                        if (err) {
-                                            // error result message
-                                            reject(err);
-                                            this.editMessageError({
-                                                chat_id: chat_id,
-                                                message_id: message_id
-                                            });
-                                            return;
-                                        }
-
-                                        // upload to dropbox
-                                        this._DropboxHelper.uploadFile({
-                                                path: '/' + path.basename(file_location),
-                                                contents: data
-                                            },
-                                            user_info.provider_sites.dropbox
-                                        ).then((upload_res) => {
-                                            // show finished message with the final URL
-                                            this.editMessage("\u{2705} Finished uploading!", {
-                                                chat_id: chat_id,
-                                                message_id: message_id
-                                            }).then((result_message) => {
-                                                // finished uploading
-                                                resolve();
-
-                                                // attempt to remove file
-                                                fs.unlink(file_location, (err) => {
-                                                    // not important if it fails
-                                                })
-                                            }).catch(reject);
-                                        }).catch(err => {
-                                            // dropbox upload failed
-                                            reject(err);
-
-                                            // error result message
-                                            this.editMessageError({
-                                                chat_id: chat_id,
-                                                message_id: message_id
-                                            });
-                                        });
-
-                                    });
-                                }).catch(reject);
-                            }).catch(err => {
-                            // download failed
-                            reject(err);
-
-                            // error result message
-                            this.editMessageError({
-                                chat_id: chat_id,
-                                message_id: message_id
-                            });
-                        });
-                    }).catch(reject);
+                    // check the cache
+                    return this.checkCache(resolveResults.storeKey)
                 })
-            }).catch(err => {
-                // get user info failed
-                reject(err);
+                // show initial status
+                .then(messageInfoTemp => {
+                    msgInfo = messageInfoTemp;
+
+                    // show status
+                    return new Promise((resolveEdit, rejectEdit) => {
+                        // set initial message status
+                        this.editMessage("\u{1F50E} Fetching file... 1/3", {
+                            chat_id: msgInfo.chat_id,
+                            message_id: msgInfo.message_id
+                        })
+                            .then(resolveEdit)
+                            .catch(rejectEdit);
+                    });
+                })
+                // download file from telegram
+                .then(result_message => this.downloadTelegram(msgInfo))
+                // update status
+                .then(file_location => {
+                    // show status
+                    return new Promise((resolveEdit, rejectEdit) => {
+                        // begin uploading to dropbox drive
+                        this.editMessage("\u{231B} Uploading to Dropbox... 2/3", {
+                            chat_id: msgInfo.chat_id,
+                            message_id: msgInfo.message_id
+                        })
+                            .then(() => resolveEdit(file_location))
+                            .catch(rejectEdit);
+                    })
+                })
+                // get file contents
+                .then(file_location => {
+                    return this.getContents(msgInfo, file_location);
+                })
+                // upload to dropbox
+                .then(resolveResults => {
+                    return this.uploadDropbox(msgInfo, resolveResults.file_contents, resolveResults.file_location, userInfo);
+                })
+                // show status
+                .then(resolveResults => {
+                    // show status message
+                    return new Promise((resolveEdit, rejectEdit) => {
+                        // show finished message with the final URL
+                        this.editMessage("\u{2705} Finished uploading!", {
+                            chat_id: msgInfo.chat_id,
+                            message_id: msgInfo.message_id
+                        })
+                            .then(() => resolveEdit(resolveResults.file_location))
+                            .catch(rejectEdit);
+                    });
+                })
+                .then(file_location => this.removeOldFile(file_location))
+                .catch(reject);
+        });
+    }
+
+    /**
+     * Get user info for a given query
+     *
+     * @param query
+     * @returns {Promise}
+     */
+    getUser(query) {
+        return new Promise((resolve, reject) => {
+            // first get user info
+            this._app._UserHelper.getUser(query.from.id)
+                .then((userInfo) => {
+                    if (!userInfo) {
+                        return resolve("It looks like you're not registered in our system.");
+                    }
+
+                    if (!userInfo.provider_sites.dropbox) {
+                        return resolve("Dropbox service not connected to your account");
+                    }
+
+                    // store key to fetch info about file
+                    const storeKey = "upload_" + query.message.chat.id +
+                        "-" + query.message.message_id;
+
+                    resolve({
+                        userInfo: userInfo,
+                        storeKey: storeKey
+                    });
+                })
+                .catch(reject);
+        })
+    }
+
+    /**
+     * Check the cache if we have info for the storeKey
+     *
+     * @param storeKey
+     * @returns {Promise}
+     */
+    checkCache(storeKey) {
+        return new Promise((resolve, reject) => {
+            // fetch from cache
+            this._app._Cache.get(storeKey, (error, msgInfo) => {
+                if (error) {
+                    return reject(error);
+                }
+                if (!msgInfo) {
+                    return reject("We couldn't find a file connected to this message. " +
+                        "Try forwarding the file to UploadBro again so he can detect it more easily.", true);
+                }
+                // resolve the retrieved message info
+                resolve(msgInfo);
+            });
+        })
+    }
+
+    /**
+     * Download file from telegram
+     *
+     * @param msgInfo
+     * @returns {Promise}
+     */
+    downloadTelegram(msgInfo) {
+        return new Promise((resolve, reject) => {
+            // download the file from telegram
+            this.downloadFile(msgInfo.file_id, msgInfo.chat_id, msgInfo.file_name)
+            // resolve new file location
+                .then((file_location) => {
+                    resolve(file_location);
+                })
+                .catch(err => {
+                    // download failed
+                    reject(err);
+
+                    // error result message
+                    this.editMessageError({
+                        chat_id: msgInfo.chat_id,
+                        message_id: msgInfo.message_id
+                    });
+                });
+        });
+    }
+
+    /**
+     * get file contents after telegram download
+     *
+     * @param msgInfo
+     * @param file_location
+     * @returns {Promise}
+     */
+    getContents(msgInfo, file_location) {
+        return new Promise((resolve, reject) => {
+            // get file contents
+            fs.readFile(file_location, (err, file_contents) => {
+                if (err || !file_contents) {
+                    // error result message
+                    reject(err);
+                    this.editMessageError({
+                        chat_id: msgInfo.chat_id,
+                        message_id: msgInfo.message_id
+                    });
+                    return;
+                }
+
+                // resolve the file contents and file location
+                resolve({
+                    file_contents: file_contents,
+                    file_location: file_location
+                })
             });
         });
+    }
+
+    /**
+     * Upload file to dropbox
+     *
+     * @param msgInfo
+     * @param file_contents
+     * @param file_location
+     * @param userInfo
+     * @returns {Promise}
+     */
+    uploadDropbox(msgInfo, file_contents, file_location, userInfo) {
+        return new Promise((resolve, reject) => {
+            // upload to dropbox
+            this._DropboxHelper.uploadFile({
+                    path: '/' + path.basename(file_location),
+                    contents: file_contents
+                },
+                userInfo.provider_sites.dropbox
+            )
+            // resolve file location to removeOldFile
+                .then((upload_result) => resolve({
+                    upload_result: upload_result,
+                    file_location: file_location
+                }))
+                .catch(err => {
+                    // dropbox upload failed
+                    reject(err);
+
+                    // error result message
+                    this.editMessageError({
+                        chat_id: msgInfo.chat_id,
+                        message_id: msgInfo.message_id
+                    });
+                });
+        })
+    }
+
+    /**
+     * Remove the file after upload
+     *
+     * @param file_location
+     * @returns {Promise}
+     */
+    removeOldFile(file_location) {
+        return new Promise((resolve, reject) => {
+            // attempt to remove file
+            fs.unlink(file_location, (err) => {
+                // not important if it fails
+            })
+
+            resolve();
+        })
     }
 
     /**
