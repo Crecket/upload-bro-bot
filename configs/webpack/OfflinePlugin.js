4330322b93691d5d9a4030fe93288d53afa03b45
crecket <crecketgaming@gmail.com>
2017-03-23 00:06:37 +0100

    caching rules

--- configs/webpack/OfflinePlugin.js
+++ configs/webpack/OfflinePlugin.js
@@ -6,19 +6,33 @@ module.exports = new OfflinePlugin({
     externals: [
         // general important files
         '/',
+        '/robots.txt',
         '/manifest.json',
-        // icons
-        '/assets/img/**.png',
-        '/assets/img/**.svg',
-        '/assets/img/google.png',
+        '/browserconfig.xml',
+        // unages
+        '/favicon-32x32.png',
+        '/favicon-16x16.png',
         '/assets/img/google.svg',
-        '/assets/img/dropbox.png',
-        '/assets/img/dropbox.svg',
-        '/assets/img/imgur.png',
         '/assets/img/imgur.svg',
+        '/assets/img/dropbox.svg',
+        '/assets/img/google.png',
+        '/assets/img/imgur.png',
+        '/assets/img/dropbox.png',
+        // icons/logos
+        '/**/*.png',
+        '/**/*.ico',
+        '/**/*.jpg',
+        '/**/*.svg',
+        '/**/*.css',
         // external files
         'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
     ],
+    caches: {
+        main: [
+            ':rest:',
+            ':externals:',
+        ]
+    },
     excludes: [
         "/api/**",
         "/new/**",
