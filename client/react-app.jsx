693fac59563f2e69934dfaa2166528e9b617e3fe
crecket <crecketgaming@gmail.com>
2017-03-30 20:25:01 +0200

    module loader test

--- client/react-app.jsx
+++ client/react-app.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import {Module} from 'react-router-server';
 
 // async loader and logger
 import ComponentLoader from './Components/Sub/ComponentLoader';
 
 // async load the main App
-const App = ComponentLoader(
-    () => import('./App'),
-    () => require.resolveWeak('./App')
-);
-
+// const App = ComponentLoader(
+//     () => import('./App'),
+//     () => require.resolveWeak('./App')
+// );
 // import App from './App';
 
 // render the react app
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(
+    <Module module={() => import('./App')}>
+        {module => module ? <module.default/> : <div>Loading...</div>}
+    </Module>
+    , document.getElementById('app'));
