e16fc500713c4b1358f63a116ded60c66349a749
Gregory Goijaerts <crecketgaming@gmail.com>
2017-04-03 11:42:12 +0200

    combined flightplan

--- flightplan.js
+++ flightplan.js
@@ -1,15 +1,17 @@
 "use strict";
 // load env config
 require('dotenv').config();
+
 // load flightplan library
 const plan = require("flightplan");
+const glob = require('glob');
 
+// server src root
 const WEB_ROOT = "/var/www/upload-bro-bot";
 
 // create a server target
 plan.target("production", {
     host: "uploadbro.com",
-    // host: "146.185.168.27",
     webRoot: WEB_ROOT,
     username: 'root',
     password: process.env.SSH_REMOTE_PASSWORD
@@ -30,24 +32,14 @@ plan.remote("update", (remote) => {
         // update yarn packages
         remote.log("Update yarn packages");
         remote.sudo("yarn");
-    });
 
-});
-
-// create a new webpack build for client-side
-plan.remote("build", (remote) => {
-    // move to correct context
-    remote.with(`cd ${WEB_ROOT}`, () => {
-        // building a new release for client side
-        remote.log("Creating new webpack build");
+        // create a new client production build with webpack
+        remote.log("Creating a new client build");
         remote.sudo("npm run build-silent");
-    });
-});
 
-// restart the node server with pm2
-plan.remote("restart", (remote) => {
-    // update yarn packages
-    remote.log("Restarting server");
-    remote.sudo("pm2 restart upload-bro-bot");
+        // update yarn packages
+        remote.log("Restarting server");
+        remote.sudo("pm2 restart upload-bro-bot");
+    });
 });
 
