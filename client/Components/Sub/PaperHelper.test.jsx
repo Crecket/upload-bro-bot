a7cb3bacca3f70b8677f086c290462d7ef82ff9b
Gregory Goijaerts <crecketgaming@gmail.com>
2017-04-06 09:42:39 +0200

    initial tests

--- client/Components/Sub/PaperHelper.test.jsx
+++ client/Components/Sub/PaperHelper.test.jsx
@@ -1,13 +1,13 @@
-import TitleBar from'./TitleBar.jsx';
+import PaperHelper from'./PaperHelper.jsx';
 
 import React from 'react';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
 
-describe('<TitleBar />', () => {
-    it('renders title text when passed', () => {
+describe('<PaperHelper />', () => {
+    it('renders children when passed', () => {
         const wrapper = shallow(
-            <TitleBar>Some Text</TitleBar>
+            <PaperHelper><h1>Some Text</h1></PaperHelper>
         );
         expect(wrapper.contains(<h1>Some Text</h1>)).to.equal(true);
     });
--- src/Helpers/Logger.test.js
+++ src/Helpers/Logger.test.js
@@ -1,30 +1,31 @@
 const assert = require('assert');
+const expect = require('chai').expect;
 
 describe('Logger', () => {
-    const Logger = require('../../src/Helpers/Logger.js');
+    const Logger = require('./Logger.js');
     describe('#trace()', () => {
-        it('should output empty string', () => {
-            Logger.trace('');
+        it('should trace function', () => {
+            expect(Logger).to.have.property('trace')
         });
     });
     describe('#debug()', () => {
-        it('should output empty string', () => {
-            Logger.debug('');
+        it('should debug function', () => {
+            expect(Logger).to.have.property('debug')
         });
     });
     describe('#info()', () => {
-        it('should output empty string', () => {
-            Logger.info('');
+        it('should info function', () => {
+            expect(Logger).to.have.property('info')
         });
     });
     describe('#warn()', () => {
-        it('should output empty string', () => {
-            Logger.warn('');
+        it('should warn function', () => {
+            expect(Logger).to.have.property('warn');
         });
     });
     describe('#error()', () => {
-        it('should output empty string', () => {
-            Logger.error('');
+        it('should error function', () => {
+            expect(Logger).to.have.property('error')
         });
     });
 });
