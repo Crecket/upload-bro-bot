e34af936f3c2faab2b14050e31de1deb7f757645
crecket <crecketgaming@gmail.com>
2017-04-09 02:09:44 +0200

    tests for all variations for logger

--- client/Helpers/ForceLogin.test.jsx
+++ client/Helpers/ForceLogin.test.jsx
@@ -1,7 +1,7 @@
 "use strict";
 import ForceLogin from'./ForceLogin.jsx';
 
-let inputParams = {
+const inputParamsDefault = {
     initialCheck: true,
     user_info: {},
     router: {
@@ -13,29 +13,49 @@ let inputParams = {
     }
 };
 
+// make 2 copies so we don't cause problems with immutability 
+let verifiedParams = Object.assign({}, inputParamsDefault);
+let unverifiedParams = Object.assign({}, inputParamsDefault, {user_info: false});
+
 describe('#ForceLogin', () => {
-    it('runs without errors', () => {
+    it('runs without errors with empty props', () => {
         ForceLogin({})
     });
 
+    // logged in scenarios
     it('logged in and redirects to dashboard', () => {
-        expect(ForceLogin(inputParams, true)).toBe(true);
+        verifiedParams.router.location.pathname = "/";
+        expect(ForceLogin(verifiedParams, true)).toBe(true);
+    });
+    it('logged in and does nothing while page is correct', () => {
+        verifiedParams.router.location.pathname = "/dashboard";
+        expect(ForceLogin(verifiedParams, true)).toBe(true);
+    });
+    it('logged in and does nothing while page is incorrect', () => {
+        verifiedParams.router.location.pathname = "/";
+        expect(ForceLogin(verifiedParams, false)).toBe(true);
+    });
+    it('logged in and does nothing while page is correct', () => {
+        verifiedParams.router.location.pathname = "/dashboard";
+        expect(ForceLogin(verifiedParams, false)).toBe(true);
     });
 
-    it('logged in and does not redirect to dashboard', () => {
-        expect(ForceLogin(inputParams, false)).toBe(true);
+    // not logged in scenarios
+    it('not logged in and does nothing while page is correct', () => {
+        unverifiedParams.router.location.pathname = "/";
+        expect(ForceLogin(unverifiedParams, true)).toBe(false);
     });
-
-    it('not logged in and redirects to home', () => {
-        inputParams.user_info = false;
-        inputParams.router.location.pathname = "/dashboard";
-        expect(ForceLogin(inputParams, true)).toBe(false);
+    it('not logged in and redirects home while page is incorrect', () => {
+        unverifiedParams.router.location.pathname = "/dashboard";
+        expect(ForceLogin(unverifiedParams, true)).toBe(false);
     });
-
-    it('not logged in and does not redirect to home', () => {
-        inputParams.user_info = false;
-        inputParams.router.location.pathname = "/dashboard";
-        expect(ForceLogin(inputParams, false)).toBe(false);
+    it('not logged in and does nothing while page is correct', () => {
+        unverifiedParams.router.location.pathname = "/";
+        expect(ForceLogin(unverifiedParams, false)).toBe(false);
+    });
+    it('not logged in and does nothing while page is incorrect', () => {
+        unverifiedParams.router.location.pathname = "/dashboard";
+        expect(ForceLogin(unverifiedParams, false)).toBe(false);
     });
 });
 
